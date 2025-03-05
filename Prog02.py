63f818d00d368efce6a1d2404f911ce3c8b5a773
AceFVA <acefrancisagustin@gmail.com>
2025-03-05 16:33:49 +0800

    Added Prog02.py

---
+++ Prog02.py
@@ -0,0 +1,7 @@
+# Prog02: Create a program that ask user to input 2 numbers. Print "Not Equal" when the numbers are not the same.
+
+num1 = int(input("Num 1: "))
+num2 = int(input("Num 2: "))
+
+if num1 != num2:
+    print("Not Equal")
