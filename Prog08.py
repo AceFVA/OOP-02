e24a30fb57c3b947caea25dde0e92f8320d96883
AceFVA <acefrancisagustin@gmail.com>
2025-03-05 18:42:49 +0800

    Added Prog08.py

---
+++ Prog08.py
@@ -0,0 +1,10 @@
+# Prog08: Create a program that print all the odd numbers starting from 0 to 100. (Use while loop)
+
+num = 0
+
+while num < 100:
+    if num % 2 == 0:
+        num += 1
+        print(num)
+    else:
+        num += 1
