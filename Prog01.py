fa147ee66823abf00f2033c6374ec4ac5b41ef91
AceFVA <acefrancisagustin@gmail.com>
2025-03-05 16:32:43 +0800

    Added Prog01.py

---
+++ Prog01.py
@@ -0,0 +1,10 @@
+#Prog01: Create a program that ask user to input 2 numbers. Print the smaller number.
+
+num1 = int(input("Num 1: "))
+num2 = int(input("Num 2: "))
+
+if num1 < num2:
+    print(num1)
+    
+else:
+    print(num2)
