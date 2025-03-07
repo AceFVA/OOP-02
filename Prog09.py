048fdadf135916f4e2822cc4bc3c40b8889fbe86
AceFVA <acefrancisagustin@gmail.com>
2025-03-07 12:57:50 +0800

    Edited Prog09.py

--- Prog09.py
+++ Prog09.py
@@ -1,5 +1,11 @@
 # Prog09: Create a program that print all the numbers starting from 0 to 100 except numbers ending in zero or ending five.
 
+for i in range(0,101):
+    if not(str(i).endswith("0") or str(i).endswith("5")):
+        print(i)
+
+'''
+Alternative
 num = 0
 
 while num < 100:
@@ -8,3 +14,4 @@ while num < 100:
         num += 1
     else:
         num += 1
+'''
