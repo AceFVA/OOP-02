6b648e0778ae291dd5d663f8ff19c7f19124d08b
AceFVA <acefrancisagustin@gmail.com>
2025-03-07 13:25:09 +0800

    Edited Prog06.py

--- Prog06.py
+++ Prog06.py
@@ -1,10 +1,10 @@
 # Prog06: Create a program that ask user to input 10 numbers. Print the result of the first number minus all of the remaining numbers.
 
-num_10 = input("Input 10 numbers separated by space: ")
-list = num_10.split()
-num1 = list[0]
+num_10 = input("Input 10 numbers (space-separated): ")
+num = num_10.split()
+num1 = num[0]
 
 for i in range(1,10):
-    num1 = int(num1) - int(list[i])
+    num1 = int(num1) - int(num[i])
     
 print(num1)
